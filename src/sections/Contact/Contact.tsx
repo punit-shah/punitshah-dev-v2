f8d68733c41af6743e6ba409af1db9d7abcbea7c
Punit Shah <pshah@turnitin.com>
2025-07-31 17:53:16 +0100

    Refactor App, add sections

---
+++ src/sections/Contact/Contact.tsx
@@ -0,0 +1,12 @@
+import Section from '../../components/Section/Section';
+
+const Contact = () => (
+  <Section className="Contact" title="Contact">
+    <p>
+      Contact form coming soon! Until then, you can reach me at{' '}
+      <a href="mailto:***REMOVED***">***REMOVED***</a>
+    </p>
+  </Section>
+);
+
+export default Contact;
---
+++ src/sections/Cover/Cover.tsx
@@ -0,0 +1,12 @@
+import Section from '../../components/Section';
+import './Cover.css';
+
+const Cover = () => (
+  <Section className="Cover" centered>
+    <h1 className="Cover-name">Punit Shah</h1>
+    <p className="Cover-lead">Software Engineer</p>
+    <hr className="Cover-divider" />
+  </Section>
+);
+
+export default Cover;
