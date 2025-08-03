4b18df30907627e6f1f624ddb154dddf494ba13b
Punit Shah <pshah@turnitin.com>
2025-08-03 05:30:01 +0100

    Remove email from contact section, replace with linkedin

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -3,8 +3,14 @@ import Section, { type CustomSectionProps } from '../../components/Section';
 const Contact = ({ ...props }: CustomSectionProps) => (
   <Section className="Contact" title="Contact" id="contact" {...props}>
     <p>
-      Contact form coming soon! Until then, you can reach me at{' '}
-      <a href="mailto:***REMOVED***">***REMOVED***</a>
+      Contact form coming soon! Until then, you can connect with me on{' '}
+      <a
+        href="https://www.linkedin.com/in/punit-shah/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        LinkedIn
+      </a>
     </p>
   </Section>
 );
