d8f392e5d55d35d60aa953c40d54689d9a44dfe7
Punit Shah <pshah@turnitin.com>
2025-08-04 20:24:35 +0100

    Add sound toggle to Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,22 @@
 import classNames from 'classnames';
-import { FolderCode, MessageCircle, Moon, Sun, UserRound } from 'lucide-react';
+import {
+  FolderCode,
+  MessageCircle,
+  Moon,
+  Sun,
+  UserRound,
+  Volume2,
+  VolumeX,
+} from 'lucide-react';
 import { createRef, useContext, useState } from 'react';
 import { DarkModeContext } from '../../contexts/DarkMode';
+import { SoundContext } from '../../contexts/Sound';
 import Logo from '../Logo';
 import Tooltip from '../Tooltip';
 import classes from './Navbar.module.css';
 import NavbarIndicator from './NavbarIndicator';
 
-const items = [
+const sectionNavItems = [
   { label: 'About', sectionId: 'about', icon: <UserRound size={32} /> },
   { label: 'Projects', sectionId: 'projects', icon: <FolderCode size={32} /> },
   { label: 'Contact', sectionId: 'contact', icon: <MessageCircle size={32} /> },
@@ -30,8 +39,24 @@ const Navbar = ({ activeSection }: NavbarProps) => {
   const [isLogoHovered, setIsLogoHovered] = useState(false);
   const [isLogoFocusVisible, setIsLogoFocusVisible] = useState(false);
   const { isDarkMode, toggleDarkMode } = useContext(DarkModeContext);
+  const { isSoundEnabled, toggleSound } = useContext(SoundContext);
 
-  const itemRefs = items.map(() => createRef<HTMLLIElement>());
+  const sectionNavRefs = sectionNavItems.map(() => createRef<HTMLLIElement>());
+
+  const settings = [
+    {
+      key: 'darkModeToggle',
+      label: isDarkMode ? 'Switch to light mode' : 'Switch to dark mode',
+      icon: isDarkMode ? <Moon size={32} /> : <Sun size={32} />,
+      onClick: toggleDarkMode,
+    },
+    {
+      key: 'soundToggle',
+      label: isSoundEnabled ? 'Disable sounds' : 'Enable sounds',
+      icon: isSoundEnabled ? <Volume2 size={32} /> : <VolumeX size={32} />,
+      onClick: toggleSound,
+    },
+  ];
 
   return (
     <nav className={classNames(classes.nav, { [classes.dark]: isDarkMode })}>
@@ -60,11 +85,11 @@ const Navbar = ({ activeSection }: NavbarProps) => {
 
       <div>
         <ul className={classes.list}>
-          {items.map((item, index) => (
+          {sectionNavItems.map((item, index) => (
             <li
               key={item.label}
               data-section={item.sectionId}
-              ref={itemRefs[index]}
+              ref={sectionNavRefs[index]}
             >
               <Tooltip
                 text={item.label}
@@ -83,17 +108,26 @@ const Navbar = ({ activeSection }: NavbarProps) => {
             </li>
           ))}
         </ul>
-        <NavbarIndicator activeSection={activeSection} itemRefs={itemRefs} />
+        <NavbarIndicator
+          activeSection={activeSection}
+          itemRefs={sectionNavRefs}
+        />
       </div>
 
-      <button
-        className={classes.button}
-        type="button"
-        aria-label={isDarkMode ? 'Switch to light mode' : 'Switch to dark mode'}
-        onClick={toggleDarkMode}
-      >
-        {isDarkMode ? <Moon size={32} /> : <Sun size={32} />}
-      </button>
+      <ul className={classes.list}>
+        {settings.map((item) => (
+          <li key={item.key}>
+            <button
+              className={classes.button}
+              type="button"
+              aria-label={item.label}
+              onClick={item.onClick}
+            >
+              {item.icon}
+            </button>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
