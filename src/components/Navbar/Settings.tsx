5ec4fcbb7e64bb3f42f59cb9a1e36a5d9f120c23
Punit Shah <pshah@turnitin.com>
2025-08-04 21:07:37 +0100

    Add sounds for dark mode toggle

--- src/components/Navbar/Settings.tsx
+++ src/components/Navbar/Settings.tsx
@@ -2,18 +2,31 @@ import { Moon, Sun, Volume2, VolumeX } from 'lucide-react';
 import { useContext } from 'react';
 import { DarkModeContext } from '../../contexts/DarkMode';
 import { SoundContext } from '../../contexts/Sound';
+import useSound from '../../hooks/useSound';
 import classes from './Navbar.module.css';
+import switchOff from './switch-off.mp3';
+import switchOn from './switch-on.mp3';
 
 const Settings = () => {
   const { isDarkMode, toggleDarkMode } = useContext(DarkModeContext);
   const { isSoundEnabled, toggleSound } = useContext(SoundContext);
+  const [playSwitchOn] = useSound(switchOn);
+  const [playSwitchOff] = useSound(switchOff);
 
   const items = [
     {
       key: 'darkModeToggle',
       label: isDarkMode ? 'Switch to light mode' : 'Switch to dark mode',
       icon: isDarkMode ? <Moon size={32} /> : <Sun size={32} />,
-      onClick: toggleDarkMode,
+      onClick: () => {
+        toggleDarkMode();
+
+        if (isDarkMode) {
+          playSwitchOff();
+        } else {
+          playSwitchOn();
+        }
+      },
     },
     {
       key: 'soundToggle',
