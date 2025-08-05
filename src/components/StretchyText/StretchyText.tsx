cc206076e4293356b00054bcd2f27fb66526c452
Punit Shah <pshah@turnitin.com>
2025-08-05 14:37:52 +0100

    Add sound effect for hover on StretchyText

--- src/components/StretchyText/StretchyText.tsx
+++ src/components/StretchyText/StretchyText.tsx
@@ -1,16 +1,24 @@
+import useSound from '../../hooks/useSound';
 import classes from './StretchyText.module.css';
+import hover from './hover.mp3';
 
 type StretchyTextProps = {
   children: React.ReactNode;
 };
 
 const StretchyText = ({ children }: StretchyTextProps) => {
+  const [playHover] = useSound(hover);
+
   if (typeof children !== 'string') {
     return children;
   }
 
   const chars = children.split('').map((char, index) => (
-    <span key={`${char}-${index}`} className={classes.char}>
+    <span
+      key={`${char}-${index}`}
+      className={classes.char}
+      onMouseEnter={() => playHover()}
+    >
       {char}
     </span>
   ));
