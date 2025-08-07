7fac65225f8657a1a548194807b342fac9463dc1
Punit Shah <pshah@turnitin.com>
2025-08-08 00:01:29 +0100

    Refactor SectionLinkIndicator to use framer motion for animations

--- src/components/Navbar/SectionLinkIndicator.tsx
+++ src/components/Navbar/SectionLinkIndicator.tsx
@@ -1,7 +1,36 @@
-import { AnimatePresence, motion } from 'framer-motion';
+import {
+  AnimatePresence,
+  motion,
+  type TargetAndTransition,
+} from 'framer-motion';
 import { useEffect, useState } from 'react';
 import useMediaQuery from '../../hooks/useMediaQuery';
 import classes from './SectionLinkIndicator.module.css';
+
+const verticalBaseStyles: TargetAndTransition = {
+  top: 0,
+  bottom: 'auto',
+  left: 0,
+  right: 'auto',
+  width: 5,
+  height: 0,
+  borderTopLeftRadius: 0,
+  borderTopRightRadius: 4,
+  borderBottomLeftRadius: 0,
+  borderBottomRightRadius: 4,
+};
+const horizontalBaseStyles: TargetAndTransition = {
+  top: 'auto',
+  bottom: 0,
+  left: 0,
+  right: 'auto',
+  width: 0,
+  height: 5,
+  borderTopLeftRadius: 4,
+  borderTopRightRadius: 4,
+  borderBottomLeftRadius: 0,
+  borderBottomRightRadius: 0,
+};
 
 type SectionLinkIndicatorProps = {
   activeSection: string | null;
@@ -13,14 +42,12 @@ const SectionLinkIndicator = ({
   itemsRef,
 }: SectionLinkIndicatorProps) => {
   const [isActive, setIsActive] = useState(false);
-  const [style, setStyle] = useState<React.CSSProperties>({});
+  const [style, setStyle] = useState<TargetAndTransition>({});
   const isVertical = useMediaQuery('(min-width: 600px)');
 
   const inactivePosition = isVertical ? { x: -5 } : { y: 5 };
   const inactive = { opacity: 0, scale: 0.8, ...inactivePosition };
-
-  const activePosition = isVertical ? { x: 0 } : { y: 0 };
-  const active = { opacity: 1, scale: 1, ...activePosition };
+  const active = { opacity: 1, scale: 1, x: 0, y: 0 };
 
   useEffect(() => {
     const updateStyle = () => {
@@ -38,19 +65,15 @@ const SectionLinkIndicator = ({
       setIsActive(true);
       if (isVertical) {
         setStyle({
+          ...verticalBaseStyles,
           top: activeItem.offsetTop,
-          left: 0,
           height: activeItem.offsetHeight,
-          borderTopLeftRadius: 0,
-          borderBottomLeftRadius: 0,
         });
       } else {
         setStyle({
+          ...horizontalBaseStyles,
           left: activeItem.offsetLeft,
-          bottom: 0,
           width: activeItem.offsetWidth,
-          borderBottomLeftRadius: 0,
-          borderBottomRightRadius: 0,
         });
       }
     };
@@ -67,11 +90,15 @@ const SectionLinkIndicator = ({
       {isActive && (
         <motion.div
           className={classes.indicator}
-          initial={inactive}
-          animate={active}
+          initial={{ ...inactive, ...style }}
+          animate={{ ...active, ...style }}
           exit={inactive}
-          transition={{ duration: 0.2 }}
-          style={style}
+          transition={{
+            type: 'spring',
+            stiffness: 500,
+            damping: 30,
+            mass: 0.3,
+          }}
         />
       )}
     </AnimatePresence>
