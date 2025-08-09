b2ecdf2191b12428e75f8a4511703eb539bcc4cd
Punit Shah <pshah@turnitin.com>
2025-08-09 05:27:48 +0100

    Add Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,61 @@
+import classNames from 'classnames';
+import { useContext } from 'react';
+import { DarkModeContext } from '../../contexts/DarkMode';
+import classes from './Input.module.css';
+
+type InputProps = {
+  label: string;
+  type?: string;
+  value?: string;
+  onChange?: (value: string) => void;
+  name?: string;
+  required?: boolean;
+  disabled?: boolean;
+};
+
+const Input = ({
+  label,
+  type = 'text',
+  value = '',
+  onChange,
+  name,
+  required,
+  disabled,
+}: InputProps) => {
+  const { isDarkMode } = useContext(DarkModeContext);
+
+  const inputProps = {
+    className: classes.input,
+    value,
+    onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+      onChange?.(e.target.value),
+    name,
+    required,
+    disabled,
+  };
+
+  const inputElement =
+    type === 'textarea' ? (
+      <textarea {...inputProps} />
+    ) : (
+      <input type={type} {...inputProps} />
+    );
+
+  return (
+    <label
+      className={classNames([
+        classes.inputContainer,
+        { [classes.dark]: isDarkMode },
+      ])}
+    >
+      {inputElement}
+      <span
+        className={classNames(classes.label, { [classes.hasValue]: value })}
+      >
+        {label}
+      </span>
+    </label>
+  );
+};
+
+export default Input;
