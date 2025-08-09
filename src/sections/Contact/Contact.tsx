4e1daa8e70669050b26f96a68494b1b2cd5a0bab
Punit Shah <pshah@turnitin.com>
2025-08-09 05:29:53 +0100

    Implement (unfinished) Contact form with styling and inputs

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -1,18 +1,45 @@
+import { useState } from 'react';
+import Input from '../../components/Input';
 import Section, { type CustomSectionProps } from '../../components/Section';
+import classes from './Contact.module.css';
 
-const Contact = ({ ...props }: CustomSectionProps) => (
-  <Section className="Contact" title="Contact" id="contact" {...props}>
-    <p>
-      Contact form coming soon! Until then, you can connect with me on{' '}
-      <a
-        href="https://www.linkedin.com/in/punit-shah/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        LinkedIn
-      </a>
-    </p>
-  </Section>
-);
+const Contact = ({ ...props }: CustomSectionProps) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  return (
+    <Section
+      containerClassName={classes.container}
+      title="Contact"
+      id="contact"
+      {...props}
+    >
+      <div className={classes.content}>
+        <form
+          className={classes.form}
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <p>
+            Have a question or just want to say hi? I’d be happy to hear from
+            you.
+          </p>
+          <Input label="Name" name="name" value={name} onChange={setName} />
+          <Input label="Email" name="email" value={email} onChange={setEmail} />
+          <Input
+            label="Message"
+            name="message"
+            value={message}
+            onChange={setMessage}
+            type="textarea"
+          />
+          {/* <Button type="submit">Send</Button> */}
+        </form>
+      </div>
+    </Section>
+  );
+};
 
 export default Contact;
