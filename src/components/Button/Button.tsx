1d711bab432e74b415e139f80aee15dda1ee5162
Punit Shah <pshah@turnitin.com>
2025-08-10 03:13:43 +0100

    Remove link props from Button since this is not used anywhere

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,18 +4,11 @@ import { DarkModeContext } from '../../contexts/DarkMode';
 import classes from './Button.module.css';
 
 type ButtonProps = {
-  type?: 'submit' | 'reset' | 'button' | 'link';
+  type?: 'submit' | 'reset' | 'button';
   className?: string;
   children: React.ReactNode;
-
-  // button props
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   disabled?: boolean;
-
-  // link props
-  href?: string;
-  target?: React.HTMLAttributeAnchorTarget;
-  rel?: string;
 };
 
 const Button = ({
@@ -24,28 +17,8 @@ const Button = ({
   children,
   onClick,
   disabled,
-  href,
-  target,
-  rel,
 }: ButtonProps) => {
   const { isDarkMode } = useContext(DarkModeContext);
-
-  if (type === 'link') {
-    return (
-      <a
-        className={classNames([
-          classes.button,
-          { [classes.dark]: isDarkMode },
-          className,
-        ])}
-        href={href}
-        target={target}
-        rel={rel}
-      >
-        {children}
-      </a>
-    );
-  }
 
   return (
     <button
