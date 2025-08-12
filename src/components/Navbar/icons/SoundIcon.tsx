4e8aeeec9fef6c8ba879ab26667f25f150e3d771
Punit Shah <pshah@turnitin.com>
2025-08-13 00:30:59 +0100

    Minor refactor for SoundIcon

--- src/components/Navbar/icons/SoundIcon.tsx
+++ src/components/Navbar/icons/SoundIcon.tsx
@@ -1,34 +1,34 @@
-import { motion } from 'framer-motion';
+import { motion, type Variants } from 'framer-motion';
 
-type SoundIconProps = { isEnabled: boolean };
-
-const waveVariants = {
+const duration = 0.15;
+const delay = 0.15;
+const waveVariants: Variants = {
   enabled: {
     opacity: 1,
     scale: 1,
-    transition: { duration: 0.15, delay: 0.15 },
+    transition: { duration, delay },
   },
   disabled: {
     opacity: 0,
     scale: 0,
-    x: -1.5,
-    transition: { duration: 0.15 },
+    x: -5,
+    transition: { duration },
   },
 };
-
-const crossVariants = {
+const xVariants: Variants = {
   enabled: (i: number) => ({
     pathLength: 0,
     opacity: 0,
-    transition: { duration: 0.15, delay: (1 - i) * 0.1 },
+    transition: { duration, delay: (1 - i) * 0.1 },
   }),
   disabled: (i: number) => ({
     pathLength: 1,
     opacity: 1,
-    transition: { duration: 0.15, delay: 0.15 + i * 0.1 },
+    transition: { duration, delay: delay + i * 0.1 },
   }),
 };
 
+type SoundIconProps = { isEnabled: boolean };
 const SoundIcon = ({ isEnabled }: SoundIconProps) => (
   <motion.svg
     xmlns="http://www.w3.org/2000/svg"
@@ -41,11 +41,13 @@ const SoundIcon = ({ isEnabled }: SoundIconProps) => (
     strokeLinecap="round"
     strokeLinejoin="round"
     role="img"
-    initial={isEnabled ? 'enabled' : 'disabled'}
+    initial={false}
     animate={isEnabled ? 'enabled' : 'disabled'}
   >
+    {/* speaker */}
     <motion.path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z" />
 
+    {/* sound waves */}
     <motion.path
       d="M16 9a5 5 0 0 1 0 6"
       style={{ originX: 0, originY: 0.5 }}
@@ -57,12 +59,13 @@ const SoundIcon = ({ isEnabled }: SoundIconProps) => (
       variants={waveVariants}
     />
 
+    {/* x */}
     <motion.line
       x1="22"
       x2="16"
       y1="9"
       y2="15"
-      variants={crossVariants}
+      variants={xVariants}
       custom={0}
     />
     <motion.line
@@ -70,7 +73,7 @@ const SoundIcon = ({ isEnabled }: SoundIconProps) => (
       x2="22"
       y1="9"
       y2="15"
-      variants={crossVariants}
+      variants={xVariants}
       custom={1}
     />
   </motion.svg>
