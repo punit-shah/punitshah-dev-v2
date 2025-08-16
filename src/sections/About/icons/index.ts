34840a1a6f2d344f9a4fadb8a05bbadffbf1ee28
Punit Shah <pshah@turnitin.com>
2025-08-16 14:20:41 +0100

    Add content for About section

---
+++ src/sections/About/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as NextJsLogo } from './NextJsLogo';
+export { default as ReactLogo } from './ReactLogo';
+export { default as StencilLogo } from './StencilLogo';
+export { default as TypeScriptLogo } from './TypeScriptLogo';
